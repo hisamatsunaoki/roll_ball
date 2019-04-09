9de28441e85ac19ae3dffa2cd3aaed9f65095e23
hisamatsunaoki <49182806+hisamatsunaoki@users.noreply.github.com>
2019-04-09 16:38:18 +0900

    チュートリアル仕様完成

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -1,25 +1,67 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed; // 動く速さ
+    public Text scoreText; // スコアの UI
+    public Text winText; // リザルトの UI
 
-    public float speed;
-
-    private Rigidbody rb;
+    private Rigidbody rb; // Rididbody
+    private int score; // スコア
 
     void Start()
     {
+        // Rigidbody を取得
         rb = GetComponent<Rigidbody>();
+
+        // UI を初期化
+        score = 0;
+        SetCountText();
+        winText.text = "";
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        // カーソルキーの入力を取得
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        // カーソルキーの入力に合わせて移動方向を設定
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
 
+        // Ridigbody に力を与えて玉を動かす
         rb.AddForce(movement * speed);
+    }
+
+    // 玉が他のオブジェクトにぶつかった時に呼び出される
+    void OnTriggerEnter(Collider other)
+    {
+        // ぶつかったオブジェクトが収集アイテムだった場合
+        if (other.gameObject.CompareTag("Pick up"))
+        {
+            // その収集アイテムを非表示にします
+            other.gameObject.SetActive(false);
+
+            // スコアを加算します
+            score = score + 1;
+
+            // UI の表示を更新します
+            SetCountText();
+        }
+    }
+
+    // UI の表示を更新する
+    void SetCountText()
+    {
+        // スコアの表示を更新
+        scoreText.text = "Count: " + score.ToString();
+
+        // すべての収集アイテムを獲得した場合
+        if (score >= 12)
+        {
+            // リザルトの表示を更新
+            winText.text = "You Win!";
+        }
     }
 }
